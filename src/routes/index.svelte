437e9b5cd607a899f5ed2132c517a49371837b94
Waleed Mumtaz <waleedthedev@gmail.com>
2022-03-04 22:30:11 +0500

    Updated meta tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,31 +7,31 @@
 <svelte:head>
 	<!-- Primary Meta Tags -->
 	<title>Shortly | URL Shortening Service</title>
-	<meta name="title" content="Shortly | URL Shortening" />
+	<meta name="title" content="Shortly | URL Shortening Service" />
 	<meta
 		name="description"
-		content="Shortly - URL Shortening service built with SvelteKit and Tailwind CSS"
+		content="https://res.cloudinary.com/dev-waleed/image/upload/v1646414927/Frontend%20Mentor/Meta%20Images/030_shortly-url-shortening_1200x628.png"
 	/>
 
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://shortly-url-shortening-two.vercel.app/" />
-	<meta property="og:title" content="Shortly | URL Shortening" />
+	<meta property="og:title" content="Shortly | URL Shortening Service" />
+	<meta property="og:description" content="" />
 	<meta
-		property="og:description"
-		content="Shortly - URL Shortening service built with SvelteKit and Tailwind CSS"
+		property="og:image"
+		content="https://res.cloudinary.com/dev-waleed/image/upload/v1646414927/Frontend%20Mentor/Meta%20Images/030_shortly-url-shortening_1200x628.png"
 	/>
-	<meta property="og:image" content="" />
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content="https://shortly-url-shortening-two.vercel.app/" />
-	<meta property="twitter:title" content="Shortly | URL Shortening" />
+	<meta property="twitter:title" content="Shortly | URL Shortening Service" />
+	<meta property="twitter:description" content="" />
 	<meta
-		property="twitter:description"
-		content="Shortly - URL Shortening service built with SvelteKit and Tailwind CSS"
+		property="twitter:image"
+		content="https://res.cloudinary.com/dev-waleed/image/upload/v1646414927/Frontend%20Mentor/Meta%20Images/030_shortly-url-shortening_1200x628.png"
 	/>
-	<meta property="twitter:image" content="" />
 </svelte:head>
 
 <div class="font-poppins">
