d99c1cc406faaf83dda0caee9fe118abd26e2b6f
Waleed Mumtaz <32681782+waleedmumtaz@users.noreply.github.com>
2022-03-04 23:29:46 +0500

    Updated meta tags description

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,14 +10,14 @@
 	<meta name="title" content="Shortly | URL Shortening Service" />
 	<meta
 		name="description"
-		content="https://res.cloudinary.com/dev-waleed/image/upload/v1646414927/Frontend%20Mentor/Meta%20Images/030_shortly-url-shortening_1200x628.png"
+		content="Shortly | URL Shortening Service built with SvelteKit and Tailwind CSS"
 	/>
 
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://shortly-url-shortening-two.vercel.app/" />
 	<meta property="og:title" content="Shortly | URL Shortening Service" />
-	<meta property="og:description" content="" />
+	<meta property="og:description" content="Shortly | URL Shortening Service built with SvelteKit and Tailwind CSS" />
 	<meta
 		property="og:image"
 		content="https://res.cloudinary.com/dev-waleed/image/upload/v1646414927/Frontend%20Mentor/Meta%20Images/030_shortly-url-shortening_1200x628.png"
@@ -27,7 +27,7 @@
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content="https://shortly-url-shortening-two.vercel.app/" />
 	<meta property="twitter:title" content="Shortly | URL Shortening Service" />
-	<meta property="twitter:description" content="" />
+	<meta property="twitter:description" content="Shortly | URL Shortening Service built with SvelteKit and Tailwind CSS" />
 	<meta
 		property="twitter:image"
 		content="https://res.cloudinary.com/dev-waleed/image/upload/v1646414927/Frontend%20Mentor/Meta%20Images/030_shortly-url-shortening_1200x628.png"
